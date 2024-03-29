252e4138c0ecff89d8e3212e8dad34989ed318f2
GharatShruti <shrutigharat62@gmail.com>
2024-03-29 12:25:31 +0530

    msg

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const taskInput = document.querySelector(".task-input input");
+taskBox = document.querySelector(".task-box");
+
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo() {
+    let li = "";
+    todos.forEach((todo,id) => {
+        console.log(id,todo);
+        li += `<li class="task">
+                <label for="${id}">
+                    <input type="checkbox" id="${id}">
+                    <p>${todo.name}</p>
+                </label>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="task-menu">
+                        <li><i class=" uil uil-pen">Edit</i></li>
+                        <li><i class=" uil uil-trash">Delete</i></li>
+                    </ul>
+                </div>
+            </li>`;
+    });
+    taskBox.innerHTML = li;
+}
+showTodo();
 
 taskInput.addEventListener("keyup", e=> {
     let userTask = taskInput.value.trim();
     if(e.key == "Enter" && userTask){
         console.log(userTask)
-        let todos = JSON.parse(localStorage.getItem("todo-list"));
         if(!todos){
             todos = [];
         }
@@ -12,5 +36,6 @@ taskInput.addEventListener("keyup", e=> {
         let taskInfo = {name: userTask,status: "pending"};
         todos.push(taskInfo); 
         localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo();
     }
 });
